0bd09a7375b74d5cdb766ba59f2054d4eeb9a097
acleary19 <annacath1125@gmail.com>
2017-03-09 08:30:00 -0500

    Add files via upload

---
+++ cookie.js
@@ -0,0 +1,15 @@
+h1{
+  text-align: center;
+  margin:100px 0px 0px 0px;
+}
+
+body {
+    background-color: #ebd6ff;}
+
+.puppy{
+  margin: 0px 0px 0px 10px;
+}
+
+.cookies{
+  margin: 0px 0px 0px 300px
+}
